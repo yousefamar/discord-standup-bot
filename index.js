9efc07a920fbd445ba167231a21fa073c44c5633
Yousef Amar <yousefamar@gmail.com>
2021-06-05 16:05:13 +0100

    add: functionality for editing standup

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ class Report {
 }
 
 let reports = [];
+let lastMessage;
+let lastTimestamp = '2021-06-03T20:00:09.128Z';
+
+const generateStandup = (usersWithoutReports, reports) => ({
+	content: 'Time for standups @everyone! DM me you report and I will put it here.',
+	embed: {
+		description: 'Remember to be high-level and concise (one sentence). Prompt:```\n1. What have you done since last standup?\n2. What do you plan to do by next standup?\n3. Any blockers?```',
+		timestamp: lastTimestamp,
+		footer: {
+			text: 'Reports',
+		},
+		fields: [
+			{
+				name: 'No reports',
+				value: usersWithoutReports.map(u => `<@${u.id}>`).join(', '),
+			},
+			...reports.map(r => r.toField()),
+		],
+	}
+});
 
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -30,23 +50,12 @@ client.on('ready', async () => {
 
 	reports = users.map(u => new Report(u, "1. adsad\n2. asdasdadsa\n3. asdasdasdaas"));
 
-	channel.send({
-		"content": "Time for standups @everyone! DM me you report and I will put it here.",
-		"embed": {
-			"description": "Remember to be high-level and concise (one sentence). Prompt:```\n1. What have you done since last standup?\n2. What do you plan to do by next standup?\n3. Any blockers?```",
-			"timestamp": "2021-06-03T20:00:09.128Z",
-			"footer": {
-				"text": "Reports"
-			},
-			"fields": [
-				{
-					"name": "No reports",
-					"value": users.map(u => `<@${u.id}>`).join(', '),
-				},
-				...reports.map(r => r.toField()),
-			]
-		}
-	});
+	lastMessage = await channel.send(generateStandup(users, reports));
+
+	setTimeout(() => {
+		reports.forEach(r => r.body = 'Hello');
+		lastMessage = lastMessage.edit(generateStandup(users, reports));
+	}, 5000);
 });
 
 client.on('message', msg => {
